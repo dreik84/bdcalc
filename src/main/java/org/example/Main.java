f12de49f0b15da38caff5748ad28234b71017f1a
dreik84 <dreik84@mail.ru>
2025-09-16 16:37:57 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Enter the operator ( + - * / ): ");
+        String operator = getString();
+
+        switch (operator) {
+            case "+":
+                System.out.print("Enter the augend ( + - * / ): ");
+                String augend = getString();
+                System.out.print("Enter the addend ( + - * / ): ");
+                String addend = getString();
+                BigDecimal sum = Calculator.addition(augend, addend);
+                System.out.println("The sum is: " + sum);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+
+    private static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
     }
 }
