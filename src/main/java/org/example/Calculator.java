1b85c2d3e43a62e5c657fe3440e3d1316503e495
dreik84 <dreik84@mail.ru>
2025-09-20 16:38:19 +0300

    Add Calculator.java

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class Calculator {
+
+    public static final MathContext MC = new MathContext(10);
+    public static final RoundingMode RM = RoundingMode.HALF_UP;
+
+    public static BigDecimal addition(String augend, String addend) {
+        BigDecimal term1 = new BigDecimal(augend, MC);
+        BigDecimal term2 = new BigDecimal(addend, MC);
+
+        return term1.add(term2, MC);
+    }
+
+    public static BigDecimal subtraction(String minuend, String subtrahend) {
+        BigDecimal term1 = new BigDecimal(minuend, MC);
+        BigDecimal term2 = new BigDecimal(subtrahend, MC);
+
+        return term1.subtract(term2, MC);
+    }
+
+    public static BigDecimal multiplication(String multiplier, String multiplicand) {
+        BigDecimal term1 = new BigDecimal(multiplier, MC);
+        BigDecimal term2 = new BigDecimal(multiplicand, MC);
+
+        return term1.multiply(term2, MC);
+    }
+
+    public static BigDecimal division(String dividend, String divisor) {
+        BigDecimal term1 = new BigDecimal(dividend, MC);
+        BigDecimal term2 = new BigDecimal(divisor, MC);
+
+        if (term2.compareTo(new BigDecimal("0", MC)) == 0)
+            throw new ArithmeticException("Деление на ноль не определено");
+
+        return term1.divide(term2, MC);
+    }
+}
